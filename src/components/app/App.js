17825eda4bf2a5c779f2b56ee3ff1f1c2646c7bc
S-KH <tadaev050@gmail.com>
2023-06-19 17:15:38 +0300

    Added routing

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,36 +1,22 @@
-import React, {useState} from 'react';
+import React from 'react';
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from '../errorBoundary/ErrorBoundary';
-
-import decoration from '../../resources/img/vision.png';
-import ComicsList from '../comicsList/ComicsList';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import MainPage from '../pages/MainPage';
+import ComicsPage from '../pages/ComicsPage';
 
 const App = () => {
-
-    const [selectedChar, setChar] = useState(null);
-
-    const onCharSelected = (id) => {
-        setChar(id);
-    }
-
     return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                <RandomChar/>
-                <div className="char__content">
-                    <CharList onCharSelected={onCharSelected}/>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-                {/*<ComicsList/>*/}
-            </main>
-        </div>
+        <BrowserRouter>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Routes>
+                        <Route path='/' element={<MainPage/>}/>
+                        <Route path='/comics' element={<ComicsPage/>}/>
+                    </Routes>
+                </main>
+            </div>
+        </BrowserRouter>
     )
 }
 
